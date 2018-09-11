f9638b833a517faa7d3bc3bea078c5e012a4ef0b
Brittany Gollins and Christabel Escarez <brittany@>
2018-09-10 20:48:44 -0700

    append contact names and show address/phone of latest entry

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,25 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
 
-    var colorChoice = $ ("#color").val();
-    var person1Input = $("input#person1").val().toUpperCase();
+    // var colorChoice = $ ("#color").val();
+    // var person1Input = $("input#person1").val().toUpperCase();
+    // var addressInput = $("input#address").val();
+    //
+    // $(".color").text(colorChoice);
+    // $(".person1").text(person1Input);
+    // $(".address").text(addressInput);
+    //
+    // $("#letter").show();
+
+    var nameInput = $("input#name").val().toUpperCase();
     var addressInput = $("input#address").val();
+    var phoneInput = $ ("input#phone").val();
 
-    $(".color").text(colorChoice);
-    $(".person1").text(person1Input);
-    $(".address").text(addressInput);
+    $("ul").prepend(nameInput);
 
-    $("#letter").show();
-
+    $("#contact-list").click(function() {
+      $(".address").text(addressInput);
+      $(".phone").text(phoneInput);
+    });
   });
 });
