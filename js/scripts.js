3a9307e616320f2ad84ef1281aea47d38ca32a89
Brittany Gollins and Christabel Escarez <christabel@>
2018-09-10 20:04:46 -0700

    change the project to a dog collar ordering page utilizing the dropdown form type

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
 
-    var person1Input = $("input#person1").val();
+    var colorChoice = $ ("#color").val();
+    var person1Input = $("input#person1").val().toUpperCase();
+    var addressInput = $("input#address").val();
 
+    $(".color").text(colorChoice);
     $(".person1").text(person1Input);
+    $(".address").text(addressInput);
 
     $("#letter").show();
 
